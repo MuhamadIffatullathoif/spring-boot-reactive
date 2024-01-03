66e6134c791db38f784c7099b7786d30dd68ee11
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-03 21:08:51 +0700

    02. Implement Repository

---
+++ src/main/java/com/iffat/springboot/reactive/repositories/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.iffat.springboot.reactive.repositories;
+
+import com.iffat.springboot.reactive.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+    Mono<Person> findById(Integer id);
+
+    Flux<Person> findAll();
+}
